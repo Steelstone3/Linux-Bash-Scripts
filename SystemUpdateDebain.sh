05ddadf2a834d12c42d52ab98993069448dc2e81
Skippy <70329398+Steelstone3@users.noreply.github.com>
2022-01-01 09:37:49 +0000

    Update SystemUpdateDebain.sh

--- SystemUpdateDebain.sh
+++ SystemUpdateDebain.sh
@@ -10,3 +10,7 @@ flatpak uninstall --unused --delete-data
 flatpak repair
 
 sudo chkrootkit
+
+alias update_linux='sudo apt update; apt list --upgradeable; sudo apt upgrade; sudo apt autopurge; sudo apt purge ~c; sudo apt autoclean; flatpak update; flatpak uninstall --unused --delete-data; flatpak repair; sudo chkrootkit;'
+
+echo "update_linux alias added to your system. This will run the commands contained in this script."
