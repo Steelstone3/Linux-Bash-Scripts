a9ee9b8d71dce9968eda257cd3a47fb8e18ad79f
Skippy <steelstone3@gmail.com>
2022-01-02 23:28:27 +0000

    refactored and added a sub menu to separate apt and flatpak package query functionality

--- PopToolbox.sh
+++ PopToolbox.sh
@@ -6,6 +6,90 @@ echo "Welcome to Pop_OS! Toolbox please select an option";
 
 #echo "update_linux alias added to your system. This will run a series of commands to update and clean your system.";
 
+updateSystem() {
+	echo "Updating packages";
+        	
+        sudo apt update;
+        apt list --upgradeable;
+        sudo apt upgrade;
+        	
+        flatpak update;
+}
+
+cleanupSystem() {
+	echo "Cleaning up Pop_OS!";
+        	
+        	sudo apt autopurge;
+        	sudo apt autoclean;
+        	
+        	flatpak uninstall --unused --delete-data;
+}
+
+upgradeSystem() {
+	echo "Upgrading to the latest Pop_OS! version";
+        	
+        sudo apt update;
+	sudo apt full-upgrade;
+	pop-upgrade release upgrade;
+}
+
+malwareScan() {
+	echo "Running chkrootkit package";
+        	
+      	sudo apt install chkrootkit;
+      		
+      	sudo chkrootkit;
+}
+
+killGraphicalEnviroment() {
+	echo "Killing Gnome Shell Session"
+
+	killall -3 gnome-shell
+}
+
+resetDisplayManager() {
+	echo "Restarting the Display Manager";
+                
+	sudo systemctl restart gdm;
+}
+
+resetNetworking() {
+	echo "Restarting the networking service";
+        	
+	sudo systemctl restart networking;
+}
+
+aptRecovery() {
+	echo "Attempting to recover apt...";
+      		
+    	sudo dpkg --configure -a;
+	sudo apt update;
+	sudo apt install --fix-broken;
+	sudo apt install --fix-missing;
+}
+
+startupServices() {
+	echo "Displaying CRITICAL chain of on boot starup services";
+        	
+	sudo systemd-analyze critical-chain;
+        	
+	echo "Use:";
+	echo "systemctl enable/disable <service>";
+	echo "To enable/disable services as daemons"
+}
+
+listAllInstalledAptPackages() {
+	echo "Listing installed apt packages";
+        	
+	apt list --installed;
+}
+
+listAllInstalledFlatpakPackages() {
+	echo "Listing installed flatpak packages";
+        	
+       	flatpak list;
+}
+
 systemManagement () {
   local PS3='Please enter your choice: '
   local options=("Pop_OS! Update System" "Pop_OS! Cleanup System" "Pop_OS! Upgrade To The Next OS Version" "Run Malware Scan" "Back")
@@ -14,35 +98,16 @@ systemManagement () {
   do
       case $opt in
           "Pop_OS! Update System")
-        	echo "Updating packages";
-        	
-        	sudo apt update;
-        	apt list --upgradeable;
-        	sudo apt upgrade;
-        	
-        	flatpak update;
+        	updateSystem
         	;;
         "Pop_OS! Cleanup System")
-        	echo "Cleaning up Pop_OS!";
-        	
-        	sudo apt autopurge;
-        	sudo apt autoclean;
-        	
-        	flatpak uninstall --unused --delete-data;
+        	cleanupSystem
         	;;
         "Pop_OS! Upgrade To The Next OS Version")
-        	echo "Upgrading to the latest Pop_OS! version";
-        	
-        	sudo apt update;
-		sudo apt full-upgrade;
-		pop-upgrade release upgrade;
+        	upgradeSystem
 		;;
 	"Run Malware Scan")
-        	echo "Running chkrootkit package";
-        	
-      		sudo apt install chkrootkit;
-      		
-      		sudo chkrootkit;
+        	malwareScan
       		;;
 	"Back")
               return
@@ -54,33 +119,22 @@ systemManagement () {
 
 systemRecovery () {
   local PS3='Please enter your choice: '
-  local options=("Kill Graphical Enviroment" "Hard Reset Of Display Manager" "Reset Network" "apt Recovery" "Back")
+  local options=("Kill Graphical Enviroment" "Hard Reset Of Display Manager" "Reset Networking" "apt Recovery" "Back")
   local opt
   select opt in "${options[@]}"
   do
       case $opt in
         "Kill Graphical Enviroment")
-	    	echo "Killing Gnome Shell Session"
-
-	   	killall -3 gnome-shell
+	    	killGraphicalEnviroment
 	        ;;
         "Hard Reset Of Display Manager")
-                echo "Restarting the Display Manager";
-                
-	    	sudo systemctl restart gdm;
+                resetDisplayManager
             	;;
-        "Reset Network")
-        	echo "Restarting the networking service";
-        	
-        	sudo systemctl restart networking;
+        "Reset Networking")
+        	resetNetworking
         	;;
         "apt Recovery")
-      		echo "Attempting to recover apt...";
-      		
-      		sudo dpkg --configure -a;
-		sudo apt update;
-		sudo apt install --fix-broken;
-		sudo apt install --fix-missing;
+      		aptRecovery
         	;;
 	"Back")
               return
@@ -98,14 +152,57 @@ performance () {
   do
       case $opt in
 	"Check On Boot Startup Services")
-		echo "Displaying CRITICAL chain of on boot starup services";
-        	
-		sudo systemd-analyze critical-chain;
-        	
-		echo "Use:";
-		echo "systemctl enable/disable <service>";
-		echo "To enable/disable services as daemons"
+		startupServices
 		;;
+	"Back")
+              return
+              ;;
+          *) echo "invalid option $REPLY";;
+      esac
+  done
+}
+
+aptQuery() {
+  local PS3='Please enter your choice: '
+  local options=("List All Installed apt Packages" "Find Installed apt Package (To Do)" "Search For Remote apt Package (To Do)" "List All Remote apt Packages (To Do)" "Back")
+  local opt
+  select opt in "${options[@]}"
+  do
+      case $opt in
+        "List All Installed apt Packages")
+        	listAllInstalledAptPackages
+        	;;
+        "Find Installed apt Package (To Do)")
+        	;;
+        "Search For Remote apt Package (To Do)")
+        	;;
+        "List All Remote apt Packages (To Do)")
+        	echo "Listing remote packages";
+        	;;
+	"Back")
+              return
+              ;;
+          *) echo "invalid option $REPLY";;
+      esac
+  done
+}
+
+flatpakQuery() {
+  local PS3='Please enter your choice: '
+  local options=("List All Installed Flatpak Packages" "Find Installed Flatpak Package (To Do)" "Search For Remote Flatpak Package (To Do)" "List All Remote Flatpak Packages (To Do)" "Back")
+  local opt
+  select opt in "${options[@]}"
+  do
+      case $opt in
+        "List All Installed Flatpak Packages")
+        	listAllInstalledFlatpakPackages
+        	;;
+        "Find Installed Flatpak Package (To Do)")
+        	;;
+        "Search For Remote Flatpak Package (To Do)")
+        	;;
+        "List All Remote Flatpak Packages (To Do)")
+        	;;
 	"Back")
               return
               ;;
@@ -116,20 +213,16 @@ performance () {
 
 packageQuery () {
   local PS3='Please enter your choice: '
-  local options=("List All Installed Packages" "Find Local Package (To Do)" "Find Remote Package (To Do)" "Back")
+  local options=("apt Package Querying" "Flatpak Package Querying" "Back")
   local opt
   select opt in "${options[@]}"
   do
       case $opt in
-        "List All Installed Packages")
-        	echo "Listing installed packages";
-        	
-        	apt list --installed;
-        	flatpak list;
+        "apt Package Querying")
+        	aptQuery
         	;;
-        "Find Local Package (To Do)")
-        	;;
-        "Find Remote Package (To Do)")
+        "Flatpak Package Querying")
+        	flatpakQuery
         	;;
 	"Back")
               return
@@ -139,27 +232,30 @@ packageQuery () {
   done
 }
 
-PS3='Please enter your choice: '
-options=("Pop_OS! Management" "Pop_OS! Recovery" "Pop_OS! Performance" "Pop_OS! Package Query"  "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-    	"Pop_OS! Management")
-    		systemManagement
-    		;;
-       	"Pop_OS! Recovery")
-       		systemRecovery
-       		;;
-       	"Pop_OS! Performance")
-       		performance
-       		;;
-       	"Pop_OS! Package Query")
-       		packageQuery
-       		;;
-        "Quit")
-            	break
-            	;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
+main() {
+	PS3='Please enter your choice: '
+	options=("Pop_OS! Management" "Pop_OS! Recovery" "Pop_OS! Performance" "Pop_OS! Package Query"  "Quit")
+	select opt in "${options[@]}"
+	do
+		case $opt in
+    			"Pop_OS! Management")
+    				systemManagement
+    				;;
+       			"Pop_OS! Recovery")
+       				systemRecovery
+       				;;
+       			"Pop_OS! Performance")
+       				performance
+       				;;
+       			"Pop_OS! Package Query")
+       				packageQuery
+       				;;
+        		"Quit")
+            			break
+            			;;
+        		*) echo "invalid option $REPLY";;
+		esac
+	done
+}
 
+main
